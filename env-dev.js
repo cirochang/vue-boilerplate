2f9c6951174c997672b6749265e5b2050e62005c
Ciro Chang <ciro.chang@tvglobo.com.br>
2018-11-27 14:27:22 -0200

    remove garbage

--- env-dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  NODE_ENV: '"development"',
-  BACKEND_API_URI_EXAMPLE: JSON.stringify("http://localhost:3000/api/v1"),
-};
--- env-prod.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  BACKEND_API_URI_EXAMPLE: JSON.stringify("/api/v1"),
-};
